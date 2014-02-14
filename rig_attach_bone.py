ed5194f640f080e7ce10148ab26c5ba71863d731
solpie <solpie@sohu.com>
2014-02-14 08:54:16 +0800

    clean

--- rig_attach_bone.py
+++ rig_attach_bone.py
@@ -15,19 +15,14 @@ def auto_weight():
     scn = bpy.context.scene
     edit_ob = bpy.context.active_object
     ob_mesh = None
-    
+
     #find ob_mesh
     obs = bpy.context.selected_objects
     for ob in obs:
-        if ob==edit_ob:
+        if ob == edit_ob:
             pass
         else:
             ob_mesh = ob
-            
-    edit_ob_name = edit_ob.name
-    # edit_ob.name = edit_ob_name +"__"
-    #print(edit_ob, ob_mesh)
-    
     ob_mesh.select = False
     bpy.ops.object.mode_set(mode='OBJECT')
     bpy.ops.object.duplicate()
@@ -36,10 +31,8 @@ def auto_weight():
     bpy.ops.armature.delete()
     bpy.ops.object.mode_set(mode='OBJECT')
 
-    
     tmp_bones = bpy.context.active_object
-    print(edit_ob, ob_mesh , tmp_bones)
-    edit_ob.name = edit_ob_name
+    print(edit_ob, ob_mesh, tmp_bones)
 
     #Auto weight
     edit_ob.select = False
@@ -48,23 +41,24 @@ def auto_weight():
     scn.objects.active = tmp_bones
     bpy.ops.object.parent_set(type='ARMATURE_AUTO')
     ob_mesh.modifiers[-1].object = bpy.data.objects[edit_ob_name]
-    
+
     edit_ob.select = True
     ob_mesh.select = True
     tmp_bones.select = False
     scn.objects.active = edit_ob
     bpy.ops.object.parent_set(type='ARMATURE', keep_transform=False)
-    
+
     #delete tmp bones
     edit_ob.select = False
     ob_mesh.select = False
     tmp_bones.select = True
-    scn.objects.active = tmp_bones 
+    scn.objects.active = tmp_bones
     bpy.ops.object.delete(use_global=False)
 
     #pose mode
     scn.objects.active = edit_ob
     bpy.ops.object.posemode_toggle()
+
 
 def attach(context):
     scn = bpy.context.scene
@@ -72,8 +66,8 @@ def attach(context):
     edit_ob = context.active_object
     obs = context.selected_objects
     for obj in obs:
-        scn.objects.active = obj 
-        if obj==edit_ob:
+        scn.objects.active = obj
+        if obj == edit_ob:
             edit_bone = context.active_bone
         else:
             target_ob = obj
@@ -81,7 +75,7 @@ def attach(context):
     # print(edit_bone, target_bone)
     def snap_head(select_head=True):
         bpy.ops.object.editmode_toggle()
-        scn.objects.active = target_ob    
+        scn.objects.active = target_ob
         target_ob.select = True
         edit_ob.select = False
         bpy.ops.object.editmode_toggle()
@@ -104,7 +98,8 @@ def attach(context):
         bpy.ops.view3d.snap_selected_to_cursor(use_offset=False)
         #set roll
         edit_bone.roll = target_bone.roll
-    #snap cursor to head
+
+        #snap cursor to head
     snap_head(True)
     snap_head(False)
 
@@ -132,7 +127,6 @@ def menu_func(self, context):
 def register():
     bpy.utils.register_module(__name__)
     bpy.types.VIEW3D_MT_armature_specials.append(menu_func)
-
 
 
 def unregister():
